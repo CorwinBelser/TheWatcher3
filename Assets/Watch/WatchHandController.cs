6f3bd6fc83e5be9d7ea6be3d2869cf00e60cd3a4
Corwin Belser <cmb539@drexel.edu>
2018-01-12 17:49:44 -0500

    Implemented watch hand snapping when the player stops input

--- Assets/Watch/WatchHandController.cs
+++ Assets/Watch/WatchHandController.cs
@@ -2,71 +2,136 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+/// Author: Corwin Belser
+/// <summary>
+/// Allows the player to move the hands of a watch forward and backward, stopping at _snapIntervalInDegrees intervals.
+/// 
+/// Unity events can be added to this script, and will be activated when their target time is selected
+/// </summary>
+/// <remarks>
+/// -   This script assumes the hour hand gameObject is the first child, the minute hand gameObject the second child object.
+/// -   The left and right arrow keys are used for movement. This is not multiplayer-safe, and should be pulled out to StartMovement(bool moveForward), and StopMovement()
+/// -   AM vs PM is not being handled. This could be added by utilizing mod with the total rotation (360 degrees being one hour)
+/// -   Holding both left and right arrow keys at the same time will only read input from the right arrow
+/// </remarks>
+/// <TODO>
+/// -   Create a hash table with watch time as the key, and a list of Unity events as the values
+///         -- Watch time could be stored and used in minute format for simplicity (Ex. 3:30 becomes 60*3 + 30 = 210
+///             --- Watch time in minutes has the range [0, 720]
+/// -   Implement snapping watch hands to the desired increment
+///         -- Watch hands should lerp to the target position
+///         -- Ideally the hands should decelerate smoothly, though using a joystick could provide better control over speed
+/// </TODO>
 public class WatchHandController : MonoBehaviour {
 
-    public float ROTATION_INCREMENT, MAX_ROTATION_SPEED;
-    /* public delegate Tuple<int, int> WatchHandListener(); */
+    public float ROTATION_INCREASE_PER_SECOND, MAX_ROTATION_PER_SECOND;
+    [Range(1,60)]
+    public int SNAP_INTERVAL_IN_MINUTES; /* Interval in minutes to snap to (0, 60] */
+    private float _snapIntervalInDegrees; /* Interval in degrees to snap to [0, 360) */
+    [Range(0.005f,0.95f)]
+    public float SNAP_THRESHOLD; /* % away from a snap interval that is acceptable to snap */
+    public float SNAP_SPEED_MULTIPLIER; /* Multiplier when rotating towards snap point when player stops input */
 
-    private Transform _minuteHand, _hourHand;
-    private float _rotationSpeed;
+    private Transform _minuteHand, _hourHand; /* References to the hour and minute hand child gameObjects */
+    private float _rotationPerSecond; /* The current amount of rotation being applied each second */
     private float _hourHandRotationMultiplier = 30f / 360f; /* The hour hand moves 30 degrees for every 360 the minute hand travels */
-    //private int[] _snapPoints = { 0, 3, 6, 9, 12 };
-    //private List<WatchHandListener> _listeners;
+    private float _rotationToMinuteValue = 60f / 360f; /* Multiply to rotation [0, 360) to get the closest minute value [0,60) */
+    private bool _isMoving;
 
-	// Use this for initialization
+	/// <summary>
+	/// Gets a reference to the hour and minute hand gameObjects attached as children
+	/// </summary>
 	void Start () {
-        _hourHand = this.transform.GetChild(0);
-        _minuteHand = this.transform.GetChild(1);
-        //_listeners = new List<WatchHandListener>();
+        _hourHand = this.transform.GetChild(0); /* The first child gameObject is expected to be the hour hand */
+        _minuteHand = this.transform.GetChild(1); /* The second child gameObject is expected to be the minute hand */
+        _snapIntervalInDegrees = 360 * SNAP_INTERVAL_IN_MINUTES / 60; /* SNAP_INTERVAL_IN_MINUTES / 60 = _snapIntervalInDegrees / 360 */
+        _isMoving = false; /* Initially no movement is applied */
 	}
 	
-	// Update is called once per frame
+	/// <summary>
+	/// Check for player input, and apply movement to the watch hands
+	/// </summary>
 	void Update () {
 		if (Input.GetKey(KeyCode.RightArrow))
         {
             /* Increment the rotation speed */
-            _rotationSpeed = Mathf.Min(MAX_ROTATION_SPEED, _rotationSpeed + ROTATION_INCREMENT * Time.deltaTime);
-
-            /*** Move the hands forward ***/
-            /* Minute hands get full effect of speed */
-            _minuteHand.RotateAround(this.transform.position, this.transform.up, _rotationSpeed * Time.deltaTime);
-
-            /* Hour hand gets a reduced rotation amount */
-            _hourHand.RotateAround(this.transform.position, this.transform.up, _hourHandRotationMultiplier * _rotationSpeed * Time.deltaTime);
+            _rotationPerSecond = Mathf.Min(MAX_ROTATION_PER_SECOND, _rotationPerSecond + ROTATION_INCREASE_PER_SECOND * Time.deltaTime);
+            _isMoving = true;
 
         } else if (Input.GetKey(KeyCode.LeftArrow))
         {
             /* Decrement the rotation speed */
-            _rotationSpeed = Mathf.Max(-1 * MAX_ROTATION_SPEED, _rotationSpeed - ROTATION_INCREMENT * Time.deltaTime);
+            _rotationPerSecond = Mathf.Max(-1 * MAX_ROTATION_PER_SECOND, _rotationPerSecond - ROTATION_INCREASE_PER_SECOND * Time.deltaTime);
+            _isMoving = true;
+        }
+        /* The player has stopped providing input. Check if the hands are at a stopping point, lerping if not */
+        else if (_isMoving)
+        {
+            Debug.Log("Snap Interval: " + _snapIntervalInDegrees + ", rotation: " + _minuteHand.localEulerAngles.y);
+            /* if minute hand is at an interval of _snapIntervalInDegrees degrees (or super close), stop moving and process watch events for this time */
+            float percentAwayFromSnap = _minuteHand.localEulerAngles.y % _snapIntervalInDegrees / _snapIntervalInDegrees;
+            Debug.Log("%: " + percentAwayFromSnap);
+            if (percentAwayFromSnap < SNAP_THRESHOLD || percentAwayFromSnap > 1 - SNAP_THRESHOLD) 
+            {
+                /* Find the closest snap interval */
+                float snapPoint = Mathf.Round(_minuteHand.localEulerAngles.y % 360 / _snapIntervalInDegrees);
+                float rotationAmount = snapPoint * _snapIntervalInDegrees - _minuteHand.localEulerAngles.y;
+                /* Rotate the minute hand to the snap point */
+                _minuteHand.RotateAround(this.transform.position, this.transform.up, rotationAmount);
 
-            /*** Move the hands backward ***/
-            /* Minute hand get full effect of speed */
-            _minuteHand.RotateAround(this.transform.position, this.transform.up, _rotationSpeed * Time.deltaTime);
+                /* Move the hour hand to its position relative to the minute hand */
+                _hourHand.RotateAround(this.transform.position, this.transform.up, _hourHandRotationMultiplier * rotationAmount);
+
+                _rotationPerSecond = 0f;
+                _isMoving = false;
+                ProcessWatchEvents();
+            }
+            else /* else  lerp towards the nearest increment of SNAP_INTERVAL_IN_DEGREES */
+            {
+                /* Find the closest snap point */
+                float snapPoint = Mathf.Round(_minuteHand.localEulerAngles.y % 360 / _snapIntervalInDegrees);
+
+                /* Set the rotation value to push the hands towards the snap point */
+                _rotationPerSecond = (Mathf.Floor(_minuteHand.localEulerAngles.y / 360) * 360 + snapPoint * _snapIntervalInDegrees - _minuteHand.localEulerAngles.y) * SNAP_SPEED_MULTIPLIER;
+            }
+        }
+
+        /* If there is rotation to be applied, move the hands */
+        if (_isMoving)
+        {
+            /* Minute hands get full effect of speed */
+            _minuteHand.RotateAround(this.transform.position, this.transform.up, _rotationPerSecond * Time.deltaTime);
 
             /* Hour hand gets a reduced rotation amount */
-            _hourHand.RotateAround(this.transform.position, this.transform.up, _hourHandRotationMultiplier * _rotationSpeed * Time.deltaTime);
-        } else
-        {
-            /* If movement has stopped, kill rotation speed, lerp the hands to the closest position */
-            _rotationSpeed = 0f;
-
-            /* Pass the time to all listeners */
-            /*Tuple<int, int> time = new Tuple<int, int>(WatchHandRotationToTime(_hourHand.rotation.y), WatchHandRotationToTime(_minuteHand.rotation.y));
-            foreach (WatchHandListener listener in _listeners)
-                listener(time);*/
+            _hourHand.RotateAround(this.transform.position, this.transform.up, _hourHandRotationMultiplier * _rotationPerSecond * Time.deltaTime);
         }
 	}
 
-    /* public void AddListener(WatchHandListener listener)
+    /// <summary>
+    /// Called when the watch stops at a _snapIntervalInDegrees
+    /// </summary>
+    /// <TODO>Trigger Unity events set for this time interval</TODO>
+    private void ProcessWatchEvents()
     {
-        _listeners.Add(listener);
-    } */
+        Debug.Log("Watch Time is " + GetWatchTimeInMinutes());
+    }
 
+    /// <summary>
+    /// Converts rotation to minute value
+    /// </summary>
+    /// <param name="rotation">Rotation in degrees of watch hand</param>
+    /// <returns>[0,60) minute value of watch hand</returns>
     private int WatchHandRotationToTime(float rotation)
     {
-        return (int)rotation % 360 / 30;
+        return Mathf.RoundToInt(rotation % 360 * _rotationToMinuteValue);
+    }
 
-        /* Snap to 15 minute increments(?) */
-        //return _snapPoints.aggregate((x, y) => Mathf.Abs(x - clampedRotation) < Mathf.Abs(y - clampedRotation) ? x : y);
+    /// <summary>
+    /// Converts the hours to minutes and added to existing minutes
+    /// </summary>
+    /// <returns>current time in minutes on the watch </returns>
+    private int GetWatchTimeInMinutes()
+    {
+        return WatchHandRotationToTime(_hourHand.localEulerAngles.y) * 60 + WatchHandRotationToTime(_minuteHand.localEulerAngles.y);
     }
 }
