fc845f7fd6c937e818578febb03fd9dafb4128f2
James Swanick <swanijam@gmail.com>
2018-01-15 19:00:57 -0500

    Adding my quick watch prototype from class, as well as my very favorite plugin OdinInspector (that's the sirenix folder) and organizing the folders so this all make sense.

---
+++ Assets/Scripts/Watch/Old_watch/MoveWatchInFrontOfCamOnSpacebar.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveWatchInFrontOfCamOnSpacebar : MonoBehaviour {
+	public Transform infrontpoint;
+	public Transform outofthewaypoint;
+
+	// -1 is lower1d
+	// 1 is raised
+	public int phase = 1;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		// toggle watch position on spacebar press
+		if (Input.GetKeyDown(KeyCode.Space)) {
+			if (phase > 0) {
+				phase = -1;
+			} else {
+				phase = 1;
+			}
+		}
+
+		// animate if in raise or lower phase. retain information about raise/lower, but shut off animation, once we get very close.
+		if (phase == 1) {
+			transform.position = Vector3.Slerp(transform.position, infrontpoint.position, Time.deltaTime * 2f);
+			if (Vector3.Distance(transform.position, infrontpoint.position) < -.01f) {
+				phase *= 2;
+			}
+		} else if (phase == -1) {
+			transform.position = Vector3.Slerp(transform.position, outofthewaypoint.position, Time.deltaTime * 2f);
+			if (Vector3.Distance(transform.position, outofthewaypoint.position) < -.01f) {
+				phase *= 2;
+			}
+		}
+	}
+}
