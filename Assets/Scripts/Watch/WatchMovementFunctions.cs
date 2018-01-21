cc9b1cb55329e2cd015558b815de74afa2ba9c00
Corwin Belser <cmb539@drexel.edu>
2018-01-21 17:50:40 -0500

    Added watch motion events

---
+++ Assets/Scripts/Watch/WatchMovementFunctions.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WatchMovementFunctions : MonoBehaviour {
+
+    public float ROTATION_PER_SECOND = 30f;
+
+    /// <summary>
+    /// Rotates the game object around the X-axis when called
+    /// </summary>
+    /// <param name="direction">The sign is used to change the direction</param>
+    public void RotateAroundX(float direction) {
+        this.transform.Rotate(new Vector3(Mathf.Sign(direction), 0, 0), ROTATION_PER_SECOND * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Watch/WatchTimeEvent.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using Sirenix.Serialization;
+using Sirenix.OdinInspector;
+
+public class WatchTimeEvent
+{
+    [Range(1, 12), SerializeField]
+    public int HOUR;
+    [Range(1, 59), SerializeField]
+    public int MINUTE;
+    [SerializeField]
+    public UnityEvent EVENT;
+
+    public int GetTimeInMinutes()
+    {
+        return this.HOUR * 60 + this.MINUTE;
+    }
+}
