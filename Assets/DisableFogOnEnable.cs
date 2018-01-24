e4f63d189cb35a468b26158650682264d0e6651c
James Swanick <swanijam@gmail.com>
2018-01-24 05:53:44 -0500

    Disabling fog on Bridge Enable, configuring watch for local(in-scene) prefabs, adding wiggle cat pro to main scene

---
+++ Assets/DisableFogOnEnable.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DisableFogOnEnable : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+	void OnEnable() {
+		RenderSettings.fog = false;
+	}
+
+	void OnDisable() {
+		RenderSettings.fog = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
